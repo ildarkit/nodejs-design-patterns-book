f5bcd65e1266de3eb5d4df9510268a7fd56a0ebb
Ildar Shamiev <shamief@yandex.ru>
2024-10-03 12:28:07 +0300

    ex5.2

--- ch05/migrateTaskQueueToAsync/src/TaskQueue.js
+++ ch05/migrateTaskQueueToAsync/src/TaskQueue.js
@@ -5,7 +5,7 @@ export class TaskQueue {
     this.queue = []
   }
 
-  runTask (task) {
+  runTask(task) {
     return new Promise((resolve, reject) => {
       this.queue.push(() => {
         return task().then(resolve, reject)
@@ -14,14 +14,16 @@ export class TaskQueue {
     })
   }
 
-  next () {
-    while (this.running < this.concurrency && this.queue.length) {
-      const task = this.queue.shift()
-      task().finally(() => {
-        this.running--
-        this.next()
-      })
-      this.running++
+  async next() {
+    while (this.running < this.concurrency && this.queue.length) { 
+      const task = this.queue.shift();
+      this.running++;
+      try {
+        await task();
+      } finally {
+        this.running--;
+        this.next();
+      }
     }
   }
 }

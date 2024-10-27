0667b954edf8156cb8f469101489a39b20dbca09
Ildar Shamiev <shamief@yandex.ru>
2024-10-27 11:33:34 +0300

    p409 components request to library api

--- ch10/library-app/src/frontend/components/pages/Author.js
+++ ch10/library-app/src/frontend/components/pages/Author.js
@@ -1,29 +1,49 @@
+import { useState, useEffect } from 'react';
+import superagent from 'superagent';
 import { html } from 'htm/react';
 import { useParams } from 'react-router-dom';
 import { FourOhFour } from './FourOhFour.js';
 import { Header } from '../Header.js';
-import { authors } from '../../../data/authors.js';
+
+async function getAuthor(id) {
+  return await superagent
+    .get(`http://localhost:3001/api/author/${id}`);
+}
 
 export function Author(props) {
   const { authorId } = useParams();
-  const author = authors.find(
-    author => author.id === authorId
+  const [loading, setLoading] = useState(true);
+  const [author, setAuthor] = useState();
+
+  useEffect(() => { 
+    getAuthor(authorId)
+      .then(({ body }) => {
+        setAuthor(body);
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
+  }, [authorId]);
+   
+  return loading ? (
+    html`<${Header}><div>Loading...</div>`
+  ) : (
+    !author ? (
+      html`<${FourOhFour}
+        staticContext=${props.staticContext}
+        error="Author not found"
+      />`
+    ) : (
+    html`<div>
+      <${Header}/>
+      <h2>${author.name}</h2>
+      <p>${author.bio}</p>
+      <h3>Books</h3>
+      <ul>
+        ${author.books.map((book) =>
+          html`<li key=${book.id}>${book.title} (${book.year})</li>`
+        )}
+      </ul>
+    </div>`
+    )
   );
-  if (!author) {
-    return html`<${FourOhFour}
-      staticContext=${props.staticContext}
-      error="Author not found"
-    />`;
-  }
-  return html`<div>
-    <${Header}/>
-    <h2>${author.name}</h2>
-    <p>${author.bio}</p>
-    <h3>Books</h3>
-    <ul>
-      ${author.books.map((book) =>
-        html`<li key=${book.id}>${book.title} (${book.year})</li>`
-      )}
-    </ul>
-  </div>`;
 }
--- ch10/library-app/src/frontend/components/pages/AuthorsIndex.js
+++ ch10/library-app/src/frontend/components/pages/AuthorsIndex.js
@@ -1,10 +1,29 @@
+import { useState, useEffect } from 'react';
 import { html } from 'htm/react';
 import { Link } from 'react-router-dom';
+import superagent from 'superagent';
 import { Header } from '../Header.js';
-import { authors } from '../../../data/authors.js';
+
+async function getAuthors() {
+  return await superagent
+    .get('http://localhost:3001/api/authors');
+}
 
 export function AuthorsIndex() {
-  return html`<div>
+  const [loading, setLoading] = useState(true);
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => {
+    getAuthors().then(({ body }) => {
+      setAuthors(body);
+      setLoading(false);
+    });
+  }, []);
+
+  return loading ? (
+    html`<${Header}><div>Loading...</div>`
+  ) : (
+  html`<div>
     <${Header}/>
     <div>${authors.map((author) =>
       html`<div key=${author.id}>
@@ -15,5 +34,6 @@ export function AuthorsIndex() {
         </p>
       </div>`)}
     </div>
-  </div>`;
+  </div>`
+  );
 }
